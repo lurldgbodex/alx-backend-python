62abc85f011161ffa6a91845d4386f7c82124ad3
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2023-01-10 14:33:41 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''coroutin loop runs 10 times and yields a random number'''
     for _ in range(10):
         await asyncio.sleep(1)
